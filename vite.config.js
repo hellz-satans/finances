03bdf555fcd030f8a7204541f99416a351eb4c42
juankman94 <juan.carlos@hey.com>
2023-08-28 21:18:04 -0600

    [npm] add vite-plugin-pwa

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 const path = require('path')
 
 /**
@@ -19,5 +20,44 @@ export default {
     outDir: 'dist/finances',
   },
 
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: 'script',
+      manifest: {
+        'name': 'My Finances',
+        'short_name': 'MyFin',
+        'description': 'Privacy-focused expenses record keeper',
+        'display': 'fullscreen',
+        'start_url': '/finances/index.html',
+        'theme_color': '#232323',
+        'background_color': '#232323',
+        'icons': [
+          {
+            'src': './img/icons/android-chrome-192x192.png',
+            'sizes': '192x192',
+            'type': 'image/png'
+          },
+          {
+            'src': './img/icons/android-chrome-512x512.png',
+            'sizes': '512x512',
+            'type': 'image/png'
+          },
+          {
+            'src': './img/icons/android-chrome-maskable-192x192.png',
+            'sizes': '192x192',
+            'type': 'image/png',
+            'purpose': 'maskable'
+          },
+          {
+            'src': './img/icons/android-chrome-maskable-512x512.png',
+            'sizes': '512x512',
+            'type': 'image/png',
+            'purpose': 'maskable'
+          }
+        ]
+      }
+    })
+  ],
 }
